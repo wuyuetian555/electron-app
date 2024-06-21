8d620447654897e3ed7e6e768840346dfd54d47c
tanghui_a <tanghui_a@aspirecn.com>
2024-06-22 04:32:55 +0800

    调整配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,11 @@ module.exports = {
   ],
   rules: {
     'vue/require-default-prop': 'off',
-    'vue/multi-word-component-names': 'off'
+    'vue/multi-word-component-names': 'off',
+    'standard/no-callback-literal': 'off',
+    'no-var': 'error', // 禁用var，用let和const代替
+    'space-before-function-paren': [0, 'always'], // 函数定义时括号前面要不要有空格
+    'eol-last': 0,
+    'no-multiple-empty-lines': [1, { max: 3 }] // 空行最多不能超过2行
   }
 }
