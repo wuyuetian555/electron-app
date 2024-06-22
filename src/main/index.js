7b4e092ecf92ebb88f9659fac608c7107189fd20
tanghui_a <tanghui_a@aspirecn.com>
2024-06-23 00:13:56 +0800

    调整代码

--- src/main/index.js
+++ src/main/index.js
@@ -1,10 +1,9 @@
-import { app, shell, BrowserWindow, ipcMain, clipboard } from 'electron'
+import { app, shell, BrowserWindow, session } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/th.jpg?asset'
 
 function createWindow() {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 900,
     height: 670,
@@ -27,8 +26,6 @@ function createWindow() {
     return { action: 'deny' }
   })
 
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
@@ -36,40 +33,30 @@ function createWindow() {
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
+  // 添加跨域请求头
+  session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+    callback({
+      responseHeaders: {
+        ...details.responseHeaders,
+        'Access-Control-Allow-Origin': ['*'],
+        'Access-Control-Allow-Methods': ['GET', 'POST'],
+        'Access-Control-Allow-Headers': ['Content-Type']
+      }
+    })
+  })
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
-
-  // IPC test
-  ipcMain.on('ping', () => console.log('pong'))
-
   createWindow()
-
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
--- src/renderer/src/views/home/index.vue
+++ src/renderer/src/views/home/index.vue
@@ -1,16 +1,20 @@
 <template>
-  <el-button>Default</el-button>
   <el-button type="primary" @click="getSelectedText">Primary</el-button>
-  <el-input v-model="value"></el-input>
-  <el-button type="success">Success</el-button>
-  <el-button type="info">Info</el-button>
-  <el-button type="warning">Warning</el-button>
-  <el-button type="danger">Danger</el-button>
+  <el-input v-model="selectedText"></el-input>
 </template>
 
 <script setup>
+import { bdTranslate } from '@/api/bdTranslate.js'
 import { ref } from 'vue'
-const value = ref('')
+const selectedText = ref('')
+const getSelectedText = async () => {
+  console.log(window.electron)
+  const result = await window.electron.ipcRenderer.send('getSelectedText')
+  console.log(result)
+  const result2 = await bdTranslate({ q: 'red and blue', to: 'zh', salt: Date.now() })
+  console.log(result2)
+  // selectedText.value = window.electronAPI.getSelectedText()
+}
 </script>
 
 <style lang="scss"></style>
